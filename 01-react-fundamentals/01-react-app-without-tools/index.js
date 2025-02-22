6affcf6669516282c690ace73677ff62f4407a2c
Sumit Attri <sumitattri165@gmail.com>
2025-02-22 20:01:13 +0530

    added create_react_app react application to main repo and moved .gitignore file to root

--- 01-react-fundamentals/01-react-app-without-tools/index.js
+++ 01-react-fundamentals/01-react-app-without-tools/index.js
@@ -1,12 +1,12 @@
 const rootElement = document.getElementById("root");
 
-// type, props, children
+// type, props/attribute, children
 // function HelloWorld() {
 //     return React.createElement("h1", { className: "heading" }, "Hello World");
 // }
 
 function HelloWorld() {
-    return <h1 className="heading">Hello World</h1>;
+  return <h1 className="heading">Hello World</h1>;
 }
 
 // h1, h2, h3, h4, div, p , article, main
