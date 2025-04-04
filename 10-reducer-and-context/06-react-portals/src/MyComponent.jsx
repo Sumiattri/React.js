1e349f79523b5438199a9e20da3afd4f46ead33c
Sumit Attri <sumitattri165@gmail.com>
2025-04-05 04:53:51 +0530

    learned about createPortal

---
+++ 10-reducer-and-context/06-react-portals/src/MyComponent.jsx
@@ -0,0 +1,22 @@
+// whatever we render in app it goes to main.jsx and then to index.html and then it renders in a div having id "root" cause that div
+// is selected in main.jsx by document.getElementById
+// what if we make a another div in index.html and then we want to render a specific component in tha that div,
+// in that case we use react-portals
+// 1 . import createPortal from react-dom
+// 2. inpsite of directly returning anything inside our component, return it by passing it as a argument to the createPortal funtion
+// 3. createPoratal fnction takes two argument, first one is anything that we want to render and second thing is the selection of id
+//    of the div in which we want to render our componet, means documeht.getElementById("name_of_id_of_the_div")
+
+import React from "react";
+import { createPortal } from "react-dom";
+
+function MyComponent() {
+  return createPortal(
+    <div>
+      <h2>MyComponent</h2>
+    </div>,
+    document.getElementById("new-root")
+  );
+}
+
+export default MyComponent;
