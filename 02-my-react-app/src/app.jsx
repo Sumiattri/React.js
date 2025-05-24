edbe0e4819f65416330e9f9b15a3fff6d0d2951b
Sumit Attri <sumitattri165@gmail.com>
2025-05-24 11:45:46 +0530

    upadated comments in revsision

--- 02-my-react-app/src/app.jsx
+++ 02-my-react-app/src/app.jsx
@@ -9,4 +9,7 @@ function App() {
   );
 }
 
+//    <></>  => these are called react fragements and are added to avoid extra container like div cause you can't render two elements
+// like h1 and p wihout a container like div
+
 export default App;
