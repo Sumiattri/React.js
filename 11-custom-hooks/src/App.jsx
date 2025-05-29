f3cf2922687af7aee972c73416c93950aaddab58
Sumit Attri <sumitattri165@gmail.com>
2025-05-29 20:02:22 +0530

    custom hooks part 1 done

--- 11-custom-hooks/src/App.jsx
+++ 11-custom-hooks/src/App.jsx
@@ -1,27 +1,10 @@
-import React from "react";
-import useFetch from "./hooks/useFetch";
+import Users from "./Users";
 
 function App() {
-  const { data, isPending, error } = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  if (isPending) {
-    return <h2>Loading.....</h2>;
-  }
-  if (error) {
-    return <h2>{error}</h2>;
-  }
   return (
-    <>
-      {data &&
-        data.map((user) => {
-          return (
-            <div key={user.id}>
-              <h2>{user.name}</h2>
-            </div>
-          );
-        })}
-    </>
+    <div>
+      <Users />
+    </div>
   );
 }
 
