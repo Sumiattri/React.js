ae6a834c758ae7c74118a4652f54ce9c9d8a7aed
Sumit Attri <sumitattri165@gmail.com>
2025-05-31 20:23:12 +0530

    route guard added on BasicDetails

--- 12-react-router-DOM-V6/src/App.jsx
+++ 12-react-router-DOM-V6/src/App.jsx
@@ -38,8 +38,16 @@ const router = createBrowserRouter(
       />{" "}
       // here RequiredAuth is acting as a route guard and Posts is the protected
       Route
-      <Route path="posts/:id" element={<PostDetails />} /> // this id is called
-      route parameter and can be accessed // in PostDetails by using useParams()
+      <Route
+        path="posts/:id"
+        element={
+          <RequiredAuth>
+            <PostDetails />
+          </RequiredAuth>
+        }
+      />{" "}
+      // this id is called route parameter and can be accessed // in PostDetails
+      by using useParams()
       <Route path="login" element={<Login />} />
       <Route path="*" element={<Error />} />
     </Route>
