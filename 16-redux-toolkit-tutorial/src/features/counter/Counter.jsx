827370e05e0490cd3edd5a9403aab9c0fb684de5
Sumit Attri <sumitattri165@gmail.com>
2025-06-05 17:31:55 +0530

    revision of counter app with redux

--- 16-redux-toolkit-tutorial/src/features/counter/Counter.jsx
+++ 16-redux-toolkit-tutorial/src/features/counter/Counter.jsx
@@ -3,6 +3,13 @@ import { increment, decrement } from "./counterSlice";
 
 function Counter() {
   const count = useSelector((state) => state.counter.count);
+  // inside store redux makes a global state oblect like
+  //   state = {
+  //            counter: { count: 0 },
+  //            xyz: { xyzState: 1 }
+  //           }
+  // this name "counter"   come from the keys you define in the reducer object when creating the store.
+
   const dispatch = useDispatch();
 
   return (
@@ -15,3 +22,24 @@ function Counter() {
 }
 
 export default Counter;
+
+// “In dispatch, we don’t pass an action object manually, we just call a function like increment() — why?”
+//
+
+//  🛠️ In Traditional Redux (Before Redux Toolkit), You used to write actions manually like this:
+
+// actions.js
+
+// export const increment = () => ({
+//   type: INCREMENT,
+// });
+
+// And then in component:
+// dispatch(increment()); // returns { type: "INCREMENT" }
+// So here, increment() was a function that returned a plain action object.
+
+// 🚀 In Redux Toolkit (RTK), When you create a slice using createSlice(), it automatically generates these action creators for you.
+// RTK automatically creates this for you behind the scenes:
+// const increment = () => ({
+//   type: "counter/increment"
+// });
--- 16-redux-toolkit-tutorial/src/store.js
+++ 16-redux-toolkit-tutorial/src/store.js
@@ -6,3 +6,5 @@ export const store = configureStore({
     counter: counterReducer,
   },
 });
+console.log(store.getState());
+// console.log(store.getState().counter);
