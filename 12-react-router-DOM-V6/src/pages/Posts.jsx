7a7017588db317b0f10669e06e9dda7ebfe354d3
Sumit Attri <sumitattri165@gmail.com>
2025-06-09 01:33:42 +0530

    modified some Loading state in router part 1

--- 12-react-router-DOM-V6/src/pages/Posts.jsx
+++ 12-react-router-DOM-V6/src/pages/Posts.jsx
@@ -1,17 +1,42 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Post from "../components/Post";
 const endpoint = "https://jsonplaceholder.typicode.com/posts";
 
 function Posts() {
   const [posts, setPosts] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [err, setErr] = useState("");
   useEffect(() => {
+    setIsLoading(true);
     async function fetchPosts() {
-      const res = await fetch(endpoint);
-      const data = await res.json();
-      setPosts(data);
+      try {
+        const res = await fetch(endpoint);
+        console.log(res);
+
+        if (!res.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          throw new Error(`${res.status} Error`);
+          return;
+        }
+        const data = await res.json();
+        setPosts(data);
+        setIsLoading(false);
+      } catch (error) {
+        setErr(error.message);
+        setIsLoading(false);
+        setIsError(true);
+      }
     }
     fetchPosts();
   }, []);
+  if (isLoading) {
+    return <p>Loading.....</p>;
+  }
+  if (isError) {
+    return <p>{err}</p>;
+  }
   return (
     <>
       {posts &&
